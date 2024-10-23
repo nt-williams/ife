8c535738a1af9f2d3afe74c8b5c26a71d1745e5f
Nicholas Williams <ntwilliams.personal@gmail.com>
2024-10-23 11:00:55 -0700

    Refactor

--- R/tidy.R
+++ R/tidy.R
@@ -5,10 +5,12 @@ generics::tidy
 tidy <- S7::new_generic("tidy", "x")
 
 S7::method(tidy, influence_func_estimate) <- function(x) {
-  out <- data.frame(estimate = x@x,
-                    std.error = x@std_error,
-                    conf.low = x@conf_int[1],
-                    conf.high = x@conf_int[2])
+  out <- data.frame(
+    estimate = x@x,
+    std.error = x@std_error,
+    conf.low = x@conf_int[1],
+    conf.high = x@conf_int[2]
+  )
   class(out) <- c("tbl_df", "tbl", "data.frame")
   out
 }
