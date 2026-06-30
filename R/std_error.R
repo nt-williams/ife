08c3945cbdf6133f54dc2761800c484075ec9347
Nicholas Williams <ntwilliams.personal@gmail.com>
2026-06-30 12:00:22 -0700

    Simplify if logic for standard error calculation

--- R/std_error.R
+++ R/std_error.R
@@ -1,17 +1,12 @@
 std_error <- function(self) {
   weighted_eif <- self@eif * self@weights
   n_clusters <- length(unique(self@id))
+  n <- length(weighted_eif)
 
-    # No clustering
-  if (n_clusters == length(weighted_eif)) {
+  # No clustering, or if only 1 cluster, treat as unclustered (all observations independent)
+  if (n_clusters == length(weighted_eif) | n_clusters == 1) {
     return(sqrt(fvar(weighted_eif) / length(weighted_eif)))
   } 
-
-  n <- length(weighted_eif)
-  # If only 1 cluster, treat as unclustered (all observations independent)
-  if (n_clusters == 1) {
-    return(sqrt(fvar(weighted_eif) / n))
-  }
   
   # Multiple clusters: use cluster-robust variance (see doi: 10.1002/sim.9813)
   cluster_sums <- fsum(weighted_eif, self@id)
