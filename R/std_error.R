69a07393ecc99ab42872a6b12d286755acf887e3
Nicholas Williams <ntwilliams.personal@gmail.com>
2026-06-30 11:57:36 -0700

    Updating cluster robust standard error calculation

--- R/std_error.R
+++ R/std_error.R
@@ -2,18 +2,19 @@ std_error <- function(self) {
   weighted_eif <- self@eif * self@weights
   n_clusters <- length(unique(self@id))
 
+    # No clustering
+  if (n_clusters == length(weighted_eif)) {
+    return(sqrt(fvar(weighted_eif) / length(weighted_eif)))
+  } 
+
+  n <- length(weighted_eif)
   # If only 1 cluster, treat as unclustered (all observations independent)
   if (n_clusters == 1) {
-    n <- length(weighted_eif)
-    return(sqrt(collapse::fvar(weighted_eif) / n))
+    return(sqrt(fvar(weighted_eif) / n))
   }
-
-  # Multiple clusters: use cluster-robust variance
-  if (n_clusters == length(weighted_eif)) {
-    cluster_means <- weighted_eif
-  } else {
-    cluster_means <- collapse::fmean(weighted_eif, self@id)
-  }
-
-  sqrt(collapse::fvar(cluster_means) / n_clusters)
+  
+  # Multiple clusters: use cluster-robust variance (see doi: 10.1002/sim.9813)
+  cluster_sums <- fsum(weighted_eif, self@id)
+  cluster_eif <- cluster_sums * n_clusters / n
+  sqrt(fvar(cluster_eif) / n_clusters)
 }
